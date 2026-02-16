ace1062750e95729373225ece9cd1080b273c084
JoeTheJust <joediamond360@gmail.com>
2026-02-16 17:37:23 -0600

    Fixed the Helper class to properly cleanse two-word cards witchout lowercasing the second word and deleting the space

--- Helper.cpp
+++ Helper.cpp
@@ -14,7 +14,16 @@ void Helper::cleanseInput(string& input) {
 	
 
 	for (int i = 0; i < input.size(); i++) {
-		if (input[i] == ' ' && isalpha(input[i + 1]) && (input.substr(0, i) == "buy" || input.substr(0, i) == "trash" || input.substr(0, i) == "discard" || input.substr(0, i) == "look" || input.substr(0, i) == "play") && !spaceFound && i + 1 < input.size()) {
+
+		if (input[i] == ' ' && i >= 7 && isalpha(input[i + 1]) && input.substr(i - 6, 6) == "Throne") {
+			input[i + 1] = toupper(input[i + 1]);
+			i++;
+		}
+		else if (input[i] == ' ' && i >= 8 && isalpha(input[i + 1]) && input.substr(i - 7, 7) == "Council") {
+			input[i + 1] = toupper(input[i + 1]);
+			i++;
+		}
+		else if (input[i] == ' ' && isalpha(input[i + 1]) && (input.substr(0, i) == "buy" || input.substr(0, i) == "trash" || input.substr(0, i) == "discard" || input.substr(0, i) == "look" || input.substr(0, i) == "play") && !spaceFound && i + 1 < input.size()) {
 			input[i + 1] = toupper(input[i + 1]);
 			i++;
 			spaceFound = !spaceFound;
